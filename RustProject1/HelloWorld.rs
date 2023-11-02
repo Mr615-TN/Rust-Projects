a2293aaffc17bcf395fb50f18d5ed3f811977cc6
Mr615-TN <tneema501@gmail.com>
2023-11-02 13:57:02 -0500

    updating code

--- RustProject1/HelloWorld.rs
+++ RustProject1/HelloWorld.rs
@@ -1,4 +1,8 @@
 fn main() {
+    // This is a comment
+    /* This is also a comment */
+    // Both ways have their applications. 
+        // For example, the first one is good for single line comments and the second one is good for multiline comments
     println!("hello world!");
     println!("Printing my first number which is {}", 8);
 }
