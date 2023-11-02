3534a1ffed7f1a9de5c714492a023535e54e637f
Mr615-TN <tneema501@gmail.com>
2023-11-02 13:58:52 -0500

    updating code

--- RustProject1/HelloWorld.rs
+++ RustProject1/HelloWorld.rs
@@ -5,4 +5,6 @@ fn main() {
         // For example, the first one is good for single line comments and the second one is good for multiline comments
     println!("hello world!");
     println!("Printing my first number which is {}", 8);
+    let random_number = /* You can use this comment in the middle of code as well */ 100;
+    println!("{}",random_number);
 }
