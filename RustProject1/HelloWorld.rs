2b29ab66e81a9af14782861ae72c4bd19ab6af93
Mr615-TN <tneema501@gmail.com>
2023-11-03 10:27:52 -0500

    adding stuff from rust

--- RustProject1/HelloWorld.rs
+++ RustProject1/HelloWorld.rs
@@ -7,4 +7,10 @@ fn main() {
     println!("Printing my first number which is {}", 8);
     let random_number = /* You can use this comment in the middle of code as well */ 100;
     println!("{}",random_number);
+    println!("{}",random_number as u8 as char);
+    println!("Size of a char: {}", std::mem::size_of::<char>()); // 4 bytes
+    println!("Size of string containing 'a': {}", "a".len()); // .len() gives the size of the string in bytes
+    println!("Size of string containing 'ß': {}", "ß".len());
+    println!("Size of string containing '国': {}", "国".len());
+    println!("Size of string containing '𓅱': {}", "𓅱".len());
 }
