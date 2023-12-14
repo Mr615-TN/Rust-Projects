456669629fd935a749fb3d266fe1b29b07eb1b10
Mr615-TN <tneema501@gmail.com>
2023-12-13 21:25:56 -0600

    update

--- RustProject1/HelloWorld.rs
+++ RustProject1/HelloWorld.rs
@@ -8,9 +8,15 @@ fn main() {
     let random_number = /* You can use this comment in the middle of code as well */ 100;
     println!("{}",random_number);
     println!("{}",random_number as u8 as char);
+    
     println!("Size of a char: {}", std::mem::size_of::<char>()); // 4 bytes
     println!("Size of string containing 'a': {}", "a".len()); // .len() gives the size of the string in bytes
     println!("Size of string containing 'ß': {}", "ß".len());
     println!("Size of string containing '国': {}", "国".len());
     println!("Size of string containing '𓅱': {}", "𓅱".len());
+
+    let slice = "Hello!";
+    println!("Slice is {} bytes and also {} characters.", slice.len(), slice.chars().count());
+    let slice2 = "안녕!";
+    println!("Slice2 is {} bytes but only {} characters.", slice2.len(), slice2.chars().count());
 }
