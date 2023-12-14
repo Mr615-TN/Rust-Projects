55647e6077760338013457cb728ad7bba26e5f51
Mr615-TN <tneema501@gmail.com>
2023-12-13 20:44:58 -0600

    update

--- RustProject2/hecto/src/main.rs
+++ RustProject2/hecto/src/main.rs
@@ -4,6 +4,10 @@ fn to_ctrl_byte(c: char) -> u8 {
     let byte = c as u8;            
     byte & 0b0001_1111            
 }
+
+fn die(e: std::io::Error) {            
+    panic!(e);            
+}         
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
     for b in io::stdin().bytes() {            
