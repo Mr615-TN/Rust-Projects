6eadb673c035ab7333aeb015726a0b13f3509d1b
Mr615-TN <tneema501@gmail.com>
2023-12-13 21:43:19 -0600

    update

--- RustProject1/Booty.rs
+++ RustProject1/Booty.rs
@@ -1,7 +1,11 @@
-fn number() -> i32 {
-    8
+fn multiply(number_one: i32, number_two: i32) {
+    let new_numero = number_one * number_two;
+    println!("{} multiplied with {} is {}", number_one, number_two, new_numero)
 }
 
 fn main() {
-    println!("Hello, world number {}!", number());
+    multiply(21, 22);
+    let other_num = 69;
+    let other_other_num = 420;
+    multiply(other_num, other_other_num);
 }
