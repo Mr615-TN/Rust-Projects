c772acbab20228de1ec3adc41021016550028578
Mr615-TN <tneema501@gmail.com>
2023-12-15 09:53:30 -0600

    update

--- RustProject1/Mutability.rs
+++ RustProject1/Mutability.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let mut new_variable = 9;
-    new_variable = 10;
-    println!("new variable = {}", new_variable);
+    let new_variable = 9;
+    println!("new variable prints {}", new_variable);
+    let new_variable = 9.2;
+    println!("new variable prints {}", new_variable);
 }
