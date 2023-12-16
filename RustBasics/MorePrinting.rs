d73a12bea52268ecaed460f2297c62bacadf1a0a
Mr615-TN <tneema501@gmail.com>
2023-12-15 21:19:38 -0600

    update

---
+++ RustBasics/MorePrinting.rs
@@ -0,0 +1,31 @@
+fn main() {
+    println!("Inside quotes
+    you can write over
+    many lines
+    and it will print just fine.");
+    
+    println!("If you forget to write
+    on the left side, the spaces
+    will be added when you print.");
+}
+
+/*
+Rust allows one to print in a multitude of ways. 
+
+One way can be like the following code:
+fn main() {
+    print!("\t You can print this on one line with a tab \n and this one another line")
+}
+
+Another thing one can do is print over many lines. The following code will show that.
+fn main() {
+    println!("Inside quotes
+    you can write over
+    many lines
+    and it will print just fine.");
+    
+    println!("If you forget to write
+    on the left side, the spaces
+    will be added when you print.");
+}
+*/
