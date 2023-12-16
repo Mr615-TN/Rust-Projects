190de46f4abf1dcaee184e7f68bf7a20b76ad376
Mr615-TN <tneema501@gmail.com>
2023-12-16 16:33:54 -0600

    update

--- RustBasics/MorePrinting.rs
+++ RustBasics/MorePrinting.rs
@@ -1,12 +1,5 @@
 fn main() {
-    println!("Inside quotes
-    you can write over
-    many lines
-    and it will print just fine.");
-    
-    println!("If you forget to write
-    on the left side, the spaces
-    will be added when you print.");
+    println!("Here are two escape characters: \\n and \\t");
 }
 
 /*
@@ -27,5 +20,10 @@ fn main() {
     println!("If you forget to write
     on the left side, the spaces
     will be added when you print.");
+}
+
+In rust, there are two escape characters adn they are \n and \t. To print them and show them in a string we would do the following:
+fn main() {
+    println!("Here are two escape characters: \\n and \\t");
 }
 */
