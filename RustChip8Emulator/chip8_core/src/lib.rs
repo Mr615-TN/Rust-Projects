20cac38df7a982e78846ea18492b2ba5f9508a94
Mr615-TN <tneema501@gmail.com>
2023-12-30 22:48:32 -0600

    update

--- RustChip8Emulator/chip8_core/src/lib.rs
+++ RustChip8Emulator/chip8_core/src/lib.rs
@@ -1,3 +1,20 @@
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+
+pub struct Emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen_size: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_registers: [u8, NUM_REGISTERS],
+    i_registers: u16,
+    sp: u16,
+    stack: [u16, STACK_SIZE]
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
