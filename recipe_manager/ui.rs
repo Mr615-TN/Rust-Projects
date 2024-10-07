e3ee0b06e19af36cf7465dab67181cc5885c0fab
Tanish Neema <tneema501@gmail.com>
2024-10-07 10:09:00 -0500

    working on recipe manager

--- recipe_manager/ui.rs
+++ recipe_manager/ui.rs
@@ -5,3 +5,28 @@ use iced::Color;
 use crate::recipe_manager::RecipeManager;
 use crate::recipe::Recipe;
 
+pub fn recipeManagerGUI() {
+    recipe_Manager: RecipeManager,
+    recipe_name: String,
+    recipe_ingredients: String,
+    recipe_instructions: String,
+    selected_recipe: Option<Recipe>,
+    error_message: Option<String>,
+    editing: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    AddRecipe,
+    EditRecipe(u32),
+    UpdateRecipe,
+    CancelEdit,
+    RecipeNameChanged(String),
+    RecipeIngredientsChanged(String),
+    RecipeInstructionsChanged(String),
+    RecipeServingsChanged(String),
+    RecipesSelected(Recipe),
+    DeleteRecipe(u32),
+    SaveRecipes,
+    LoadRecipes,
+}
