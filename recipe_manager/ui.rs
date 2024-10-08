9be1696d122b3c17ac619bae0469729a801ee2d8
Mr615-TN <tneema501@gmail.com>
2024-10-08 13:02:56 -0500

    working on recipe_manager

--- recipe_manager/ui.rs
+++ recipe_manager/ui.rs
@@ -56,7 +56,7 @@ impl Sandbox for recipeManagerGUI {
                     let servings = self.recipe_servings.parse().unwrap_or(1);
                     self.recipe_Manager.add_Recipe(
                         self.recipe_name.clone(),
-                        self.recipe_ingredients.split(', ').map(String::from).collect(),
+                        self.recipe_ingredients.split(',').map(String::from).collect(),
                         self.recipe_instructions.split('\n').map(String::from).collect(),
                         servings,
                         );
@@ -71,8 +71,103 @@ impl Sandbox for recipeManagerGUI {
                     self.recipe_name = recipe.name.clone();
                     self.recipe_ingredients = recipe.ingredients.join(", ");
                     self.recipe_instructions = recipe.instructions.join("\n");
+                    self.recipe_servings = recipe.servings.to_string();
+                    self.selected_recipe = Some(recipe.clone());
+                    self.editing = true;
+                }
+            }
+            Message::UpdateRecipe => {
+                if let Some(recipe) = &.selected_recipe {
+                    let servings = self.recipe_servings.parse().unwrap_or(recipe.servings);
+                    self.recipe_Manager.update_Recipe(
+                        recipe.id,
+                        self.recipe_name.clone(),
+                        self.recipe_ingredients.split(',').map(String::from).collect(),
+                        self.recipe_instructions.split('\n').map(String::from).collect(),
+                        servigns
+                        );
+                    self.editing = false;
+                    self.selected_recipe = None;
+                }
+            }
+            Message::CancelEdit => {
+                self.editing = false;
+                self.recipe_name.clear();
+                self.recipe_ingredients.clear();
+                self.recipe_instructions.clear();
+                self.recipe_servings.clear();
+            }
+            Message::RecipeNameChanged(name) => {
+                self.recipe_name = name;
+            }
+            Message::RecipeIngredientsChanged(ingredients) => {
+                self.recipe_ingredients = ingredients;
+            }
+            Message::RecipeInstructionsChanged(instructions) => {
+                self.recipe_instructions = instructions;
+            }
+            Message::RecipeServingsChanged(servings) => {
+                self.recipe_servings = servings;
+            }
+            Message::RecipesSelected(recipe) => {
+                self.selected_recipe = Some(recipe);
+                self.editing =false;
+            }
+            Message::DeleteRecipe(id) => {
+                if self.recipe_Manager.delete_Recipe(id) {
+                    self.selected_recipe = None;
+                } 
+            }
+            Message::SaveRecipes => {
+                if let Err(e) = self.recipe_Manager.save_ToFile("recipes.json") {
+                    self.error_message = Some(format!("Failed to save recipes: {}", e))
+                }
+            }
+            Message::LoadRecipes => {
+                match self.recipe_Manager.load_FromFile("recipes.json") {
+                    Ok(_) => self.selected_recipe = None,
+                    Err(e) => self.error_message = Some(format!("Failed to load recipes: {}", e))
                 }
             }
         }
     }
-}
+    fn view(&self) -> Element<Message> {
+        let recipe form = Column::new()
+        .push(TextInput::new(
+            "Enter recipe name ....",
+            &self.recipe_name,
+            Message::RecipeNameChanged,
+        ))
+        .push(TextInput::new(
+            "Enter ingredients (comma separated) ....",
+            &self.recipe_ingredients,
+            Message::RecipeIngredientsChanged,
+        ))
+        .push(TextInput::new(
+            "Enter instructions (line separated) ....",
+            &self.recipe_instructions,
+            Message::RecipeInstructionsChanged,
+        ))
+        .push(TextInput::new(
+            "Enter servings ....",
+            &self.recipe_servings,
+            Message::RecipeServingsChanged,
+        ))
+        .push(
+            if self.editing {
+                Button::new(Text::new("Update Recipe")).on_press(Message::UpdateRecipe)
+            }
+            else {
+                Button::new(Text::new("Add Recipe")).on_press(Message::AddRecipe)
+            }
+        )
+        .push(
+            if self.editing {
+                Button::new(Text::new("Cancel")).on_press(Message::CancelEdit)
+            }
+            else {
+                Button::new(Text::new(""))
+            }
+        );
+        let recipes: Element<_> = self
+    }
